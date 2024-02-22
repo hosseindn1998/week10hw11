dc8573026c52e8c041346f83ee1256246538a562
hosseindn1998 <hosseindn1998@gmail.com>
2024-02-22 08:20:12 +0330

    create 5 thread and set priority for them and start

--- src/main/java/StarvationQ3.java
+++ src/main/java/StarvationQ3.java
@@ -8,7 +8,27 @@ class StarvationQ3 extends Thread {
     }
 
     public static void main(String[] args) {
+        System.out.println(" thread 2 is starting");
 
+        StarvationQ3 thread1 = new StarvationQ3();
+        thread1.setPriority(9);
+        StarvationQ3 thread2 = new StarvationQ3();
+        thread2.setPriority(7);
+        StarvationQ3 thread3 = new StarvationQ3();
+        thread3.setPriority(5);
+        StarvationQ3 thread4 = new StarvationQ3();
+        thread4.setPriority(3);
+        StarvationQ3 thread5 = new StarvationQ3();
+        thread5.setPriority(1);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+
+        System.out.println("thread 2 is finishing");
+    }
     }
 
 }
