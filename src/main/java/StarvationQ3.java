a58dfa301331e33e035af7d80d548a399b4f8314
hosseindn1998 <hosseindn1998@gmail.com>
2024-02-22 08:18:52 +0330

    in the name of god ; first commit

--- src/main/java/StarvationQ3.java
+++ src/main/java/StarvationQ3.java
@@ -7,26 +7,8 @@ class StarvationQ3 extends Thread {
         threadcount++;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(" thread 2 is starting");
+    public static void main(String[] args) {
 
-        StarvationQ3 thread1 = new StarvationQ3();
-        thread1.setPriority(9);
-        StarvationQ3 thread2 = new StarvationQ3();
-        thread2.setPriority(7);
-        StarvationQ3 thread3 = new StarvationQ3();
-        thread3.setPriority(5);
-        StarvationQ3 thread4 = new StarvationQ3();
-        thread4.setPriority(3);
-        StarvationQ3 thread5 = new StarvationQ3();
-        thread5.setPriority(1);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-
-        System.out.println("thread 2 is finishing");
     }
+
 }
